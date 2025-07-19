b789ee0092b42a48b506a686130078b09a5b4a9e
AnthonyP57 <antonipawlowicz123@gmail.com>
2025-07-19 19:44:59 +0200

    pretraining data

--- Radovid_model/pretraining_data.py
+++ Radovid_model/pretraining_data.py
@@ -2,83 +2,91 @@ from datasets import load_dataset
 import json
 import re
 
-pretraining = './training_datasets/pretrainig.jsonl'
-pretraining_ = './training_datasets/pretrainig_overview.jsonl'
+pretraining = './training_datasets/pretraining.jsonl'
+pretraining_ = './training_datasets/pretraining_overview.jsonl'
 
 """"""
-# dataset = load_dataset("roneneldan/TinyStories", split='validation')
+dataset = load_dataset("roneneldan/TinyStories", split='train')
 
-# data = {'metadata': {'dataset':"roneneldan/TinyStories", 'split':'validation'},
-#         'data': dataset[:]}
+data = {'metadata': {'dataset':"roneneldan/TinyStories", 'split':'train'},
+        'data': dataset[:]}
 
-# with open(pretraining_, 'a') as f:
-#     json.dump({'metadata': {'dataset':"roneneldan/TinyStories", 'split':'validation'},
-#         'num_rows':dataset.num_rows}, f)
+with open(pretraining_, 'a') as f:
+    json.dump({'metadata': {'dataset':"roneneldan/TinyStories", 'split':'train'},
+        'num_rows':dataset.num_rows}, f)
 
-# with open(pretraining, 'a') as f:
-#     json.dump(data, f)
+with open(pretraining, 'a') as f:
+    json.dump(data, f)
 
+print(f'for dataset {data["metadata"]["dataset"]}, {data["metadata"]["split"]} has {dataset.num_rows} rows\n sample: {data["data"][0][:200]}')
 """"""
-# dataset = load_dataset("roneneldan/TinyStories", split='train')
+dataset = load_dataset("roneneldan/TinyStoriesInstruct", split='train[:10%]')
 
-# data = {'metadata': {'dataset':"roneneldan/TinyStories", 'split':'train'},
-#         'data': dataset[:]}
+data = {'metadata': {'dataset':"roneneldan/TinyStoriesInstruct", 'split':'train[:10%]'},
+        'data': []}
 
-# with open(pretraining_, 'a') as f:
-#     json.dump({'metadata': {'dataset':"roneneldan/TinyStories", 'split':'train'},
-#         'num_rows':dataset.num_rows}, f)
+with open(pretraining_, 'a') as f:
+    json.dump({'metadata': {'dataset':"roneneldan/TinyStoriesInstruct", 'split':'train[:10%]'},
+        'num_rows':dataset.num_rows}, f)
 
-# with open(pretraining, 'a') as f:
-#     json.dump(data, f)
+for text in dataset:
+    text = text['text']
+    if not bool(re.match(r'^(\w+:|<\|)', text)) and len(text) > 10:
+        data['data'].append(text)
 
-
-""""""
-# dataset = load_dataset("roneneldan/TinyStoriesInstruct", split='train[:10%]')
-
-# data = {'metadata': {'dataset':"roneneldan/TinyStoriesInstruct", 'split':'train[:10%]'},
-#         'data': []}
-
-# with open(pretraining_, 'a') as f:
-#     json.dump({'metadata': {'dataset':"roneneldan/TinyStoriesInstruct", 'split':'train[:10%]'},
-#         'num_rows':dataset.num_rows}, f)
-
-# for text in dataset:
-#     text = text['text']
-#     if not bool(re.match(r'^(\w+:|<\|)', text)) and len(text) > 10:
-#         data['data'].append(text)
-
-# with open(pretraining, 'a') as f:
-#     json.dump(data, f)
+with open(pretraining, 'a') as f:
+    json.dump(data, f)
 
 """"""
 
-# dataset = load_dataset("nyu-mll/glue", "mnli", split="train")
+dataset = load_dataset("nyu-mll/glue", "mnli", split="train")
 
-# with open(pretraining_, 'a') as f:
-#     json.dump({'metadata': {'dataset':"nyu-mll/glue", 'subset':'mnli', 'split':'train'},
-#         'num_rows':dataset.num_rows}, f)
+with open(pretraining_, 'a') as f:
+    json.dump({'metadata': {'dataset':"nyu-mll/glue", 'subset':'mnli', 'split':'train'},
+        'num_rows':dataset.num_rows}, f)
 
-# data = {'metadata': {'dataset':"nyu-mll/glue", "subset":"mnli", 'split':'train'},
-#         'data': []}
+data = {'metadata': {'dataset':"nyu-mll/glue", "subset":"mnli", 'split':'train'},
+        'data': []}
 
-# for text in dataset:
-#     data['data'].append(text['premise'])
-#     data['data'].append(text['hypothesis'])
+for text in dataset:
+    data['data'].append(text['premise'])
+    data['data'].append(text['hypothesis'])
 
-# with open(pretraining, 'a') as f:
-#     json.dump(data, f)
+with open(pretraining, 'a') as f:
+    json.dump(data, f)
 
 """"""
 
-# dataset = load_dataset("SimpleStories/SimpleStories", split="train[:1%]")
+dataset = load_dataset("SimpleStories/SimpleStories", split="train")
 
-# data = {'metadata': {'dataset':"SimpleStories/SimpleStories", 'split':'train'},
-#         'data': dataset[:]}
+data = {'metadata': {'dataset':"SimpleStories/SimpleStories", 'split':'train'},
+        'data': []}
 
-# with open(pretraining_, 'a') as f:
-#     json.dump({'metadata': {'dataset':"SimpleStories/SimpleStories", 'split':'train'},
-#         'num_rows':dataset.num_rows}, f)
+with open(pretraining_, 'a') as f:
+    json.dump({'metadata': {'dataset':"SimpleStories/SimpleStories", 'split':'train'},
+        'num_rows':dataset.num_rows}, f)
 
-# with open(pretraining, 'a') as f:
-#     json.dump(data, f)
+for text in dataset:
+    data['data'].append(text['story'])
 
+with open(pretraining, 'a') as f:
+    json.dump(data, f)
+
+""""""
+dataset = load_dataset("Helsinki-NLP/opus-100", "en-sv", split="train")
+
+data = {'metadata': {'dataset':"Helsinki-NLP/opus-100", 'subset':'en-sv', 'split':'train'},
+        'data': dataset[:]}
+
+with open(pretraining_, 'a') as f:
+    json.dump({'metadata': {'dataset':"Helsinki-NLP/opus-100", "subset":"en-sv", 'split':'train'},
+        'num_rows':dataset.num_rows}, f)
+
+data = {'metadata': {'dataset':"Helsinki-NLP/opus-100", "subset":"en-sv", 'split':'train'},
+        'data': []}
+
+for text in dataset:
+    data['data'].append(text['translation']['en'])
+
+with open(pretraining, 'a') as f:
+    json.dump(data, f)
