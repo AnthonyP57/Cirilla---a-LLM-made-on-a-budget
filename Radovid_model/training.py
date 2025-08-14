e45ba2ed66998fa4eb5127ccf8ca214b927abc93
AnthonyP57 <antonipawlowicz123@gmail.com>
2025-08-14 15:03:48 +0200

    training class

--- Radovid_model/training.py
+++ Radovid_model/training.py
@@ -1,50 +1,133 @@
-import time
-import numpy as np
 import torch
 import torch.nn as nn
-from model import Radovid, Args
+from model import Radovid
 from torch.utils.data import DataLoader
+from functools import partial
+from dataclasses import dataclass, field
+from torch.optim import Optimizer, AdamW
 
-def train_radovid(n_epochs:int, model:Radovid, args:Args, dataloader:DataLoader, optim:torch.optim=None, lr:float=5e-5, **optim_kwargs) -> Radovid:
+@dataclass
+class TrainingArgs:
+    n_epoch:int = 100
+    optim:Optimizer = AdamW
+    lr:float = 5e-5
+    xavier_init:bool = True
+    optim_kwargs:dict[str,str] = field(default_factory=lambda: {'fused':True, 'foreach':False})
 
-    torch.set_float32_matmul_precision('high')
-    torch.backends.cudnn.benchmark = True
-    torch._dynamo.config.capture_scalar_outputs = True
+class RadovidTrainer:
+    def __init__(self, model:Radovid, training_args:TrainingArgs):
+        self.model = model
+        self.args = training_args
+        self.optim = self._prepare_optimizer(**training_args.optim_kwargs)
 
-    model = Radovid(args)
+        print(f'n trainable params: {(model.n_params/1e6):.2f} M')
 
-    for param in model.parameters():
-        if param.dim() > 1:
-            nn.init.xavier_uniform_(param)
+    def train(self, dataloader:DataLoader):
 
-    print(f'{(model.n_params/1e6):.2f} M')
+        self._set_global_vars()
 
-    criterion = torch.nn.CrossEntropyLoss()
+        if self.args.xavier_init:
+            self._xavier_init()
 
-    if optim is None:
-        optimizer_dict = {p: torch.optim.AdamW([p], fused=True, foreach=False, lr=lr) for p in model.parameters()}
-    else:
-        optimizer_dict = {p: optim([p], lr=lr, **optim_kwargs) for p in model.parameters()}
+        self._fuse_optim()
 
-    def optimizer_hook(parameter) -> None:
-        optimizer_dict[parameter].step()
-        optimizer_dict[parameter].zero_grad(set_to_none=True)
+        self._set_prior_training_vars()
 
-    for p in model.parameters():
-        p.register_post_accumulate_grad_hook(optimizer_hook)
+        criterion = nn.CrossEntropyLoss()
 
+        for epoch in range(self.args.n_epoch):
 
-    torch._inductor.config.triton.cudagraph_skip_dynamic_graphs = True
-    torch._inductor.config.triton.cudagraph_dynamic_shape_warn_limit = None
+            for x, y in dataloader:
 
-    for epoch in range(n_epochs):
-        for x, y in dataloader:
+                torch.compiler.cudagraph_mark_step_begin()
+                out = self.model.pred(x)
+                loss = criterion(out.view(-1, self.model.args.vocab_size), y.view(-1))
 
+                loss.backward()
+
+    def benchmark(self):
+        
+        self._set_global_vars()
+
+        x = torch.randint(0, self.model.args.vocab_size,
+                          (4, self.model.args.context_window),
+                          dtype=torch.long, device=self.model.args.device)
+        
+        y = torch.randint(0, self.model.args.vocab_size,
+                          (4, self.model.args.context_window),
+                          dtype=torch.long, device=self.model.args.device)
+
+        if self.args.xavier_init:
+            self._xavier_init()
+
+        self._fuse_optim()
+
+        self._set_prior_training_vars()
+
+        criterion = nn.CrossEntropyLoss()
+
+        for i in range(5): #warm up for benchmark
             torch.compiler.cudagraph_mark_step_begin()
             out = model.pred(x)
-            loss = criterion(out.view(-1, args.vocab_size), y.view(-1))
+            loss = criterion(out.view(-1, self.model.args.vocab_size), x.view(-1))
 
             loss.backward()
 
+        torch.cuda.synchronize()
+        
+        times = []
+
+        for i in range(100):
+            torch.compiler.cudagraph_mark_step_begin()
+            out = model.pred(x)
+            loss = criterion(out.view(-1, self.model.args.vocab_size), y.view(-1))
+            loss_item = loss.item()
+
+            loss.backward()
             
-            
+            times.append(time.time())
+            print(f'average time: {np.mean(np.diff(times)):.4f} loss: {loss_item}', end='\r')
+
+        print(f'average time for epoch: {np.mean(np.diff(times)):.4f}')
+
+    def _xavier_init(self):
+        for param in self.model.parameters():
+            if param.dim() > 1:
+                nn.init.xavier_uniform_(param)
+
+    @staticmethod
+    def _set_global_vars():
+        torch.set_float32_matmul_precision('high')
+        torch.backends.cudnn.benchmark = True
+        torch._dynamo.config.capture_scalar_outputs = True
+    
+    @staticmethod
+    def _set_prior_training_vars():
+        torch._inductor.config.triton.cudagraph_skip_dynamic_graphs = True
+        torch._inductor.config.triton.cudagraph_dynamic_shape_warn_limit = None
+
+    def _prepare_optimizer(self, **optim_kwargs):
+        return partial(self.args.optim, **optim_kwargs, lr=self.args.lr)
+    
+    def _fuse_optim(self):
+        optimizer_dict = {p: self.optim([p]) for p in self.model.parameters()}
+
+        def optimizer_hook(parameter) -> None:
+            # if parameter.grad is not None:
+            #     parameter.grad.data.clamp_(-1, 1)
+            optimizer_dict[parameter].step()
+            optimizer_dict[parameter].zero_grad(set_to_none=True)
+
+        for p in self.model.parameters():
+            p.register_post_accumulate_grad_hook(optimizer_hook)
+
+if __name__ == '__main__':
+    import time
+    import numpy as np
+    from model import Args
+
+    model = Radovid(Args())
+
+    trainer = RadovidTrainer(model, TrainingArgs())
+
+    trainer.benchmark()
