b5f11eff9c92f22f04d27c3a895c77b7283ece11
AnthonyP57 <antonipawlowicz123@gmail.com>
2025-11-01 18:36:52 +0100

    reptile

--- cirilla/Few_shot/Maml_trainer.py
+++ cirilla/Few_shot/Maml_trainer.py
@@ -2,6 +2,224 @@ import torch.nn as nn
 import torch
 from torch.utils.data import DataLoader, TensorDataset
 
+class ReptileTrainer:
+    def __init__(self,
+                model,
+                tokenizer,
+                meta_lr=0.001,
+                inner_lr=0.01,
+                num_inner_steps=5,
+                max_len=512):
+        """
+        Args:
+            model: Transformer model
+            device: Compute device
+            meta_lr: Learning rate for meta-optimization
+            inner_lr: Learning rate for inner loop adaptation
+            num_inner_steps: Number of inner loop adaptation steps
+        """
+
+        self.tokenizer = tokenizer
+        self.model = model
+
+        self.device = getattr(model.args, 'device', model.device)
+
+        self.inner_lr = inner_lr
+        self.num_inner_steps = num_inner_steps
+
+        self.meta_optimizer = torch.optim.Adam(model.parameters(), lr=self.meta_lr)
+
+        self.loss_fn = nn.BCELoss()
+        self.max_len = max_len
+
+    def _get_embeddings(self, texts: list[str]) -> torch.Tensor:
+        """
+        Get embeddings from model model
+
+        Args:
+            texts: list of text strings
+
+        Returns:
+            embeddings: Tensor of embeddings
+        """
+        inputs = self.tokenizer(texts, return_tensors='pt', padding='max_length', truncation=True, max_length=self.max_len)
+
+        with torch.no_grad():
+            embeddings = self.model(inputs['input_ids'], inputs['attention_mask'])
+
+        return embeddings
+    
+    def meta_train(self,
+                    meta_train_tasks: list[dict[str, list]],
+                    meta_valid_tasks: list[dict[str, list]] = None,
+                    epochs: int = 100):
+        """
+        Meta-training loop
+
+        Args:
+            meta_train_tasks: list of tasks for meta-training
+            meta_valid_tasks: list of tasks for meta-validation
+            epochs: Number of meta-training epochs
+        """
+        losses = []
+        losses_valid = []
+
+        for epoch in range(epochs):
+            meta_train_loss = 0
+            n=0
+
+            for task in meta_train_tasks:
+                for n_iter, batch in enumerate(task):
+                    support_data = batch['support_data']
+                    support_labels = torch.tensor(batch['support_labels'], dtype=torch.float32).to(self.device)
+                    query_data = batch['query_data']
+                    query_labels = torch.tensor(batch['query_labels'], dtype=torch.float32).to(self.device)
+
+                    if support_labels.shape[0] == 0 or query_labels.shape[0] == 0:
+                        if (n_iter+1) != task.n_parts:
+                            print(f"Skipping empty task: {task.name} at iter: {n_iter+1}/{task.n_parts}")
+
+                        continue
+
+                    supports = self.tokenizer(support_data, return_tensors='pt', padding='max_length', truncation=True, max_length=self.max_len)
+                    support_ids, support_masks = supports['input_ids'], supports['attention_mask']
+
+                    querys = self.tokenizer(query_data, return_tensors='pt', padding='max_length', truncation=True, max_length=self.max_len)
+                    query_ids, query_masks = querys['input_ids'], querys['attention_mask']
+
+                    ids = torch.cat((support_ids, query_ids), dim=0)
+                    masks = torch.cat((support_masks, query_masks), dim=0)
+                    labels = torch.cat((support_labels, query_labels), dim=0)
+
+                    adapted_model = type(self.model)(*self.model._modules.values()).to(self.device)
+                    adapted_model.load_state_dict(self.model.state_dict())
+                    inner_optimizer = torch.optim.SGD(adapted_model.parameters(), lr=self.inner_lr)
+                    
+                    for _ in range(self.num_inner_steps):
+
+                        predictions = adapted_model(ids, masks)
+
+                        inner_loss = self.loss_fn(predictions.view(-1), labels)
+
+                        inner_optimizer.zero_grad()
+                        inner_loss.backward(retain_graph=True)
+                        inner_optimizer.step()
+
+                    with torch.no_grad():
+                        original_param_dict = self.model.state_dict()
+                        adapted_param_dict = adapted_model.state_dict()
+
+                        assert original_param_dict.keys() == adapted_param_dict.keys()
+
+                        for key in original_param_dict.keys():
+                            original_param_dict[key] = original_param_dict[key] + self.meta_lr * (adapted_param_dict[key] - original_param_dict[key])
+
+                        self.model.load_state_dict(original_param_dict)
+
+                    meta_train_loss += inner_loss.item()
+                    n+=1
+
+                print(f"Epoch {epoch+1}, Meta-Train Loss: {meta_train_loss/n:.4f}")
+                losses.append(meta_train_loss/n)
+
+    def fine_tune(self,
+                    train_texts: list[str],
+                    train_labels: list[int],
+                    test_texts: list[str],
+                    test_labels: list[int],
+                    epochs: int = 50,
+                    batch_size: int = 16,
+                    learning_rate: float = 1e-2,
+                    verbose: bool = False):
+        """
+        Fine-tune the model on a specific downstream task
+
+        Args:
+            train_texts: list of training texts strings
+            train_labels: list of training labels
+            test_texts: list of test texts strings
+            test_labels: list of test labels
+            epochs: Number of fine-tuning epochs
+            batch_size: Batch size for training
+            learning_rate: Learning rate for fine-tuning
+            verbose: Whether to print training progress
+
+        Returns:
+            Trained MAMLChemBERTa model
+        """
+        tokenized_train = self.tokenizer(train_texts, return_tensors='pt', padding='max_length', truncation=True, max_length=self.max_len)
+        tokenized_test = self.tokenizer(test_texts, return_tensors='pt', padding='max_length', truncation=True, max_length=self.max_len)
+
+        train_ids = tokenized_train['input_ids']
+        train_masks = tokenized_train['attention_mask']
+
+        test_ids = tokenized_test['input_ids']
+        test_masks = tokenized_test['attention_mask']
+
+        train_labels_tensor = torch.tensor(train_labels, dtype=torch.float32)
+        test_labels_tensor = torch.tensor(test_labels, dtype=torch.float32)
+
+        train_dataset = TensorDataset(train_ids, train_masks, train_labels_tensor)
+        train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+
+        test_dataset = TensorDataset(test_ids, test_masks, test_labels_tensor)
+        test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+
+        self.classifier.train()
+        for epoch in range(epochs):
+            train_loss = 0
+            valid_loss = 0
+            nt = 0
+            nv = 0
+            for phase in ['train', 'valid']:
+                if phase == 'train':
+                    for batch_ids, masks, labels in train_loader:
+
+                        adapted_model = type(self.model)(*self.model._modules.values()).to(self.device)
+                        adapted_model.load_state_dict(self.model.state_dict())
+                        inner_optimizer = torch.optim.SGD(adapted_model.parameters(), lr=self.inner_lr)
+
+                        for _ in range(self.num_inner_steps):
+
+                            predictions = adapted_model(batch_ids, masks)
+
+                            inner_loss = self.loss_fn(predictions, labels)
+
+                            inner_optimizer.zero_grad()
+                            inner_loss.backward(retain_graph=True)
+                            inner_optimizer.step()
+
+                        b = batch_ids.shape[0]
+                        nt += b
+                        train_loss += inner_loss.item()*b
+
+                        with torch.no_grad():
+                            original_param_dict = self.model.state_dict()
+                            adapted_param_dict = adapted_model.state_dict()
+
+                            assert original_param_dict.keys() == adapted_param_dict.keys()
+
+                            for key in original_param_dict.keys():
+                                original_param_dict[key] = original_param_dict[key] + learning_rate * (adapted_param_dict[key] - original_param_dict[key])
+
+                            self.model.load_state_dict(original_param_dict)
+                    
+                else:
+                    with torch.no_grad():
+                        for batch_ids, masks, labels in train_loader:
+
+                            predictions = self.model(batch_ids, masks)
+                            loss = self.loss_fn(predictions, labels)
+
+                            b = batch_ids.shape[0]
+                            nv += b
+                            valid_loss += loss.item()*b
+
+            if verbose:
+                print(f"Epoch {epoch}, train loss: {train_loss/nt:.4f}, valid loss: {valid_loss/nv:.4f}")
+
+        return self
+
 class MAMLBinaryAdapterTrainer:
     def __init__(self,
                 model,
