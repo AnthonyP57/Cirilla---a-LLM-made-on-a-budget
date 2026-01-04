b88acb2c7f06038df9669ce5f34e6a686cd0e4ed
AnthonyP57 <antonipawlowicz123@gmail.com>
2026-01-04 17:06:33 +0100

    GRPO cirilla sampler with per token log probs

--- cirilla/RL/GRPO/generate_answers.py
+++ cirilla/RL/GRPO/generate_answers.py
@@ -1,6 +1,10 @@
 from cirilla.Cirilla_model import Cirilla
 from cirilla.Cirilla_model import CirillaTokenizer
 from cirilla.Cirilla_model.modules import select_torch_device
+from datasets import Dataset
+import json
+import torch
+import math
 
 class CirillaResponseGenerator:
     def __init__(self, hub_url):
@@ -62,53 +66,25 @@ class CirillaResponseGenerator:
         self.model.clear_cache()
         return texts
 
-if __name__ == '__main__':
-    import json
-    import math
-    import torch
+class CirillaSampler:
+    def __init__(self, crg: CirillaResponseGenerator):
+        self.crg = crg
 
-    crg = CirillaResponseGenerator('AnthonyPa57/Cirilla-0.3B-4E')
+    def sample(self, prompt_dataset:Dataset, batch_size:int=32, n_generate_with_kv_cache:int=1, n_generate_naive:int=2):
 
-    prompts_path = './training_datasets/RL/prompts.jsonl'
-    out_path = './training_datasets/RL/sampled_cirilla.jsonl'
-    out = []
-    j = 0
-    prompts = {}
+        crg = self.crg
 
-    for line in open(prompts_path, 'r'):
-        line = json.loads(line)
-        id = line['id']
-        prompts[id] = line
-        if id > 3:
-            break
-        prompt = line['prompt']
-        answers_naive = crg.generate_batch(prompt, 2, kv_cache=False)
+        out = []
+        j = 0
 
-        for ans in answers_naive:
-            out.append({
-                'id': id,
-                'answer': ans,
-                'log_probs_id':j
-            })
-            j += 1
+        for line in prompt_dataset:
+            id = line['id']
+            if id > 3:
+                break
+            prompt = line['prompt']
+            answers_naive = crg.generate_batch(prompt, n=n_generate_naive, kv_cache=False)
 
-    batch_size = 32
-    with open(prompts_path, 'r') as f:
-        for _ in range(math.ceil(len(out) / batch_size)):
-            ids = []
-            prompts = []
-            for _ in range(batch_size):
-                line = f.readline()
-                if line != '':
-                    line = json.loads(line)
-                    if line['id'] > 3:
-                        break
-                    ids.append(line['id'])
-                    prompts.append(line['prompt'])
-
-            answers_kv = crg.generate_batch(prompts, kv_cache=True)
-
-            for ans, id in zip(answers_kv, ids):
+            for ans in answers_naive:
                 out.append({
                     'id': id,
                     'answer': ans,
@@ -116,51 +92,85 @@ if __name__ == '__main__':
                 })
                 j += 1
 
-    with open(out_path, 'w') as f:
-        for d in out:
-            json.dump(d, f)
-            f.write('\n')
+        for _ in range(n_generate_with_kv_cache):
+            _prompt_dataset = iter(prompt_dataset)
+            for _ in range(math.ceil(len(out) / batch_size)):
+                ids = []
+                prompts = []
+                for _ in range(batch_size):
+                    line = next(_prompt_dataset)
+                    if line != '':
+                        if line['id'] > 3:
+                            break
+                        ids.append(line['id'])
+                        prompts.append(line['prompt'])
 
-    out_tensor = []
-    generate_batch = []
-    prompt_lens = []
-    js = []
+                answers_kv = crg.generate_batch(prompts, kv_cache=True)
 
-    last_out = out[-1]['id']
-    for ans_data in out:
-        prompt = prompts[ans_data['id']]
-        answer = ans_data['answer']
-        j = ans_data['log_probs_id']
-        end = ans_data['id'] == last_out
-        template = crg.tokenizer.apply_chat_template(
-                [
-                    {'role':'user', 'content': prompt},
-                    {'role':'assistant', 'content': answer}
-                ],
+                for ans, id in zip(answers_kv, ids):
+                    out.append({
+                        'id': id,
+                        'answer': ans,
+                        'log_probs_id':j
+                    })
+                    j += 1
+                    
+        out.sort(key=lambda x: x['id'])
+        return Dataset.from_list(out)
+    
+    def get_log_probs(self, prompt_dataset:Dataset, sampled_dataset:Dataset, batch_size:int=32):
+
+        out_tensor = []
+        generate_batch = []
+        prompt_lens = []
+        js = []
+
+        last_out = sampled_dataset[-1]['id']
+        for ans_data in sampled_dataset:
+            prompt = prompt_dataset[ans_data['id']]['prompt']
+            answer = ans_data['answer']
+            j = ans_data['log_probs_id']
+            end = ans_data['id'] == last_out
+            template = self.crg.tokenizer.apply_chat_template(
+                    [
+                        {'role':'user', 'content': prompt},
+                        {'role':'assistant', 'content': answer}
+                    ],
+                    padding='do_not_pad',
+                    add_generation_prompt=False
+                )
+            
+            prompt_len = len(self.crg.tokenizer.apply_chat_template(
+                [{'role':'user', 'content': prompt}],
                 padding='do_not_pad',
-                add_generation_prompt=False
-            )
-        
-        prompt_len = len(crg.tokenizer.apply_chat_template(
-            [{'role':'user', 'content': prompt}],
-            padding='do_not_pad',
-            add_generation_prompt=True
-        ))
-        js.append(j)
-        generate_batch.append(template)
-        prompt_lens.append(prompt_len)
-        if len(generate_batch) == batch_size or end:
-            x = torch.full((len(generate_batch), max(len(t) for t in generate_batch)), crg.tokenizer.convert_tokens_to_ids('<pad>'), dtype=torch.int64) # one is padding
-            for i, t in enumerate(generate_batch):
-                x[i, :len(t)] = torch.tensor(t, dtype=torch.int64)
-            per_token_log_probs = crg.model.get_per_token_log_probs(x.to(crg.model.args.device))
-            for j, row_ptlp, plen, batch in zip(js, per_token_log_probs, prompt_lens, generate_batch):
-                out_tensor.append({'log_probs_id':j, 'per_token_log_probs':row_ptlp[plen-1:len(batch)-1].tolist()})
-            generate_batch = []
-            prompt_lens = []
-            js = []
+                add_generation_prompt=True
+            ))
+            js.append(j)
+            generate_batch.append(template)
+            prompt_lens.append(prompt_len)
+            if len(generate_batch) == batch_size or end:
+                x = torch.full((len(generate_batch), max(len(t) for t in generate_batch)), self.crg.tokenizer.convert_tokens_to_ids('<pad>'), dtype=torch.int64) # one is padding
+                for i, t in enumerate(generate_batch):
+                    x[i, :len(t)] = torch.tensor(t, dtype=torch.int64)
+                per_token_log_probs = self.crg.model.get_per_token_log_probs(x.to(self.crg.model.args.device))
+                for j, row_ptlp, plen, batch in zip(js, per_token_log_probs, prompt_lens, generate_batch):
+                    out_tensor.append({'log_probs_id':j, 'per_token_log_probs':row_ptlp[plen-1:len(batch)-1].tolist()})
+                generate_batch = []
+                prompt_lens = []
+                js = []
 
-    with open('./training_datasets/RL/per_token_log_probs.jsonl', 'w') as f:
-        for d in out_tensor:
-            json.dump(d, f)
-            f.write('\n')
+        return Dataset.from_list(out_tensor)
+
+if __name__ == '__main__':
+    from datasets import load_dataset
+    crg = CirillaResponseGenerator('AnthonyPa57/Cirilla-0.3B-4E')
+
+    sampler = CirillaSampler(crg)
+
+    prompt_dataset = load_dataset('AnthonyPa57/Witcher-GRPO-prompts', split='train')
+
+    sampled_dataset = sampler.sample(prompt_dataset, batch_size=32, n_generate_with_kv_cache=2)
+    sampled_dataset.to_json('./training_datasets/RL/sampled_cirilla.jsonl')
+
+    log_probs_dataset = sampler.get_log_probs(prompt_dataset, sampled_dataset, batch_size=32)
+    log_probs_dataset.to_json('./training_datasets/RL/per_token_log_probs.jsonl')
