e0a832673a369af6ee947a6898b2917a11a304cb
AnthonyP57 <antonipawlowicz123@gmail.com>
2026-05-10 22:06:09 +0200

    prepared pretrain code

--- cirilla_training/cirilla_02b_mtp/pre_training.py
+++ cirilla_training/cirilla_02b_mtp/pre_training.py
@@ -1,13 +1,13 @@
 import torch
 import torch.nn.functional as F
-from cirilla.Cirilla_model import Cirilla, Args, load_balancing_loss, get_optims
+from cirilla.Cirilla_model import CirillaMTP, Args, load_balancing_loss, get_optims
 from cirilla.Cirilla_model import CirillaTrainer, TrainingArgs, CirillaTokenizer, JSONLDataset
 from types import MethodType
 
 
 hf_repo = 'AnthonyPa57/Cirilla-0.3B-4E'
 
-model = Cirilla(Args(
+model = CirillaMTP(Args(
                     output_moe_weights=True,
                     out_bias=True,
                     tie_params=True,
@@ -46,42 +46,50 @@ muon_opt, adam_opt = get_optims(
 
 micro_batch_size = 8
 
-def cirilla_grad_acc(self, data):
-
-    x = data[0]
-    y = data[1]
-
-    train_loss = torch.zeros(1, device=x.device)
+def mtp_training_step_grad_acc(self, data) -> float:
+    step_loss = 0.0
+    n = 0
 
     torch.compiler.cudagraph_mark_step_begin()
+    
+    x = data[0]
+    y = data[1]
+    
+    y_fill = torch.tensor([[pad_token_id] * self.model.args.n_token_heads] * y.shape[0], dtype=y.dtype, device=y.device)
+    y = torch.hstack([y, y_fill])
 
     n_micro_steps = max(1, x.size(0) // micro_batch_size)
 
     for micro_step in range(n_micro_steps):
 
-        # clear_router_zloss()
-        # clear_load_balancing_loss()
-
         x_ = x[micro_step*micro_batch_size:(micro_step+1)*micro_batch_size]
         y_ = y[micro_step*micro_batch_size:(micro_step+1)*micro_batch_size]
 
-        logits, moe_weight_list = model.pred(x_)
-        # logits = model.pred(x)
-
+        # z = self.model.get_z(x_)
+        z, moe_weight_list = self.model.get_z(x_)
         lb_losses = [
             load_balancing_loss(w, num_experts=model.args.num_experts, top_k=model.args.k)
             for w in moe_weight_list
                 ]
         lb_loss = torch.stack(lb_losses).mean()
 
-        loss = (F.cross_entropy(
-            logits.view(-1, logits.size(-1)), y_.view(-1), ignore_index=pad_token_id, label_smoothing=0.1
-        # ) + 0.1 * batched_router_zloss(model.smoes[0].args).mean()) / n_micro_steps
-        ) + 0.01 * lb_loss) / n_micro_steps
+        zd = z.detach()
+        zd.requires_grad = True
 
-        train_loss += loss.detach()
+        for i in range(self.model.args.n_token_heads):
+            preds = self.model.get_heads(i, zd)
 
-        loss.backward()
+            loss = (F.cross_entropy(
+                preds.view(-1, self.model.args.vocab_size),
+                y_[:, i:-(self.model.args.n_token_heads - i)].reshape(-1),
+                ignore_index=pad_token_id, label_smoothing=0.1) + (0.01 * lb_loss / self.model.args.n_token_heads)\
+                    ) / n_micro_steps
+            
+            step_loss += loss.item()
+            n += 1
+            loss.backward()
+
+        z.backward(gradient=zd.grad)
 
     muon_opt.step()
     adam_opt.step()
@@ -89,44 +97,35 @@ def cirilla_grad_acc(self, data):
     muon_opt.zero_grad(set_to_none=True)
     adam_opt.zero_grad(set_to_none=True)
 
-    return train_loss.item()
+    return step_loss / n
 
 @torch.inference_mode()
-def cirilla_grad_acc_inference(self, data):
+def mtp_inference_step_grad_acc(self, data) -> float:
+    step_loss = 0.0
+    n = 0
 
     x = data[0]
     y = data[1]
-
-    train_loss = torch.zeros(1, device=x.device)
-
+    
     n_micro_steps = max(1, x.size(0) // micro_batch_size)
 
     for micro_step in range(n_micro_steps):
 
-        # clear_router_zloss()
-        # clear_load_balancing_loss()
-
         x_ = x[micro_step*micro_batch_size:(micro_step+1)*micro_batch_size]
         y_ = y[micro_step*micro_batch_size:(micro_step+1)*micro_batch_size]
 
-        logits, moe_weight_list = model.pred(x_)
-        # logits = model.pred(x)
+        z, moe_weight_list = self.model.get_z(x_)
 
-        # lb_losses = [
-        #     load_balancing_loss(w, num_experts=model.args.num_experts, top_k=model.args.k)
-        #     for w in moe_weight_list
-        #         ]
-        # lb_loss = torch.stack(lb_losses).mean()
+        preds = self.model.get_heads(0, z)
+        loss = F.cross_entropy(
+            preds.view(-1, self.model.args.vocab_size),
+            y_.reshape(-1),
+            ignore_index=pad_token_id) / n_micro_steps
+            
+        step_loss += loss.item()
+        n += 1
 
-        loss = (F.cross_entropy(
-            logits.view(-1, logits.size(-1)), y_.view(-1), ignore_index=pad_token_id
-        # ) + 0.1 * batched_router_zloss(model.smoes[0].args).mean()) / n_micro_steps
-        # ) + 0.01 * lb_loss) / n_micro_steps
-        )) / n_micro_steps
-
-        train_loss += loss.detach()
-
-    return train_loss.item()
+    return step_loss
 
 dl = JSONLDataset(
                 './training_datasets/pretraining/pretraining.jsonl',
@@ -149,8 +148,8 @@ trainer = CirillaTrainer(model,
                                         )
                                     )
 
-trainer.training_step = MethodType(cirilla_grad_acc, trainer)
-trainer.inference_step = MethodType(cirilla_grad_acc_inference, trainer)
+trainer.training_step = MethodType(mtp_training_step_grad_acc, trainer)
+trainer.inference_step = MethodType(mtp_inference_step_grad_acc, trainer)
 trainer.criterion = None
 trainer.optims_to_save = {'muon_opt': muon_opt, 'adam_opt': adam_opt}
 
